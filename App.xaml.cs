707ea2b036248621311fa976cd1b26a3e644e6f8
Oana Istoc <o.istoc@outlook.com>
2023-11-29 15:58:35 +0200

    Add Shopping List

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace Istoc_Oana_Lab7
+﻿using Istoc_Oana_Lab7.Data;
+using System;
+using System.IO;
+
+namespace Istoc_Oana_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();

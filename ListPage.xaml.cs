47514c3d0e00356960785ad9e65585d35e48cc7c
Oana Istoc <o.istoc@outlook.com>
2023-12-05 15:51:49 +0200

    Add ProdusPage

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -8,18 +8,34 @@ public partial class ListPage : ContentPage
     {
         InitializeComponent();
     }
-
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
-        var slist = (ShopList)BindingContext;
-        slist.Date = DateTime.UtcNow;
-        await App.Database.SaveShopListAsync(slist);
-        await Navigation.PopAsync();
+        var product = (Product)BindingContext;
+        await App.Database.SaveProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
     }
     async void OnDeleteButtonClicked(object sender, EventArgs e)
     {
-        var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
-        await Navigation.PopAsync();
+        var product = (Product)BindingContext;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
---
+++ ProductPage.xaml.cs
@@ -0,0 +1,49 @@
+namespace Istoc_Oana_Lab7;
+using Istoc_Oana_Lab7.Models;
+
+public partial class ProductPage : ContentPage
+{
+    public ShopList sl;
+
+    public ProductPage(ShopList slist)
+    {
+		InitializeComponent();
+        sl = slist;
+    }
+    async void OnAddButtonClicked(object sender, EventArgs e)
+    {
+        Product p;
+        if (listView.SelectedItem != null)
+        {
+            p = listView.SelectedItem as Product;
+            var lp = new ListProduct()
+            {
+                ShopListID = sl.ID,
+                ProductID = p.ID
+            };
+            await App.Database.SaveListProductAsync(lp);
+            p.ListProducts = new List<ListProduct> { lp };
+            await Navigation.PopAsync();
+        }
+    }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.SaveProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+
+
+}
